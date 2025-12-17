fc0f0f627190001328efb90af8c1493e74ad7602
Erik Slovák <erik.slovak7@gmail.com>
2025-12-17 13:08:21 +0100

    Add Vercel Analytics (#7)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 // import { PostHogProvider } from "./providers";
+import { Analytics } from "@vercel/analytics/react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
             {children}
           {/* </PostHogProvider> */}
         </main>
+        <Analytics />
       </body>
     </html>
   );
